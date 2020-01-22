af9da6d3f93ee44e2a407bd5ac6a381e6d52097b
Erica Schimmel <ericaschimmel01@gmail.com>
2020-01-22 17:41:33 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -11,7 +11,7 @@ def oxford_comma(array)
   string_list =""
   
   array.each do |element|
-    if element == array.first ||
+    if element == array.first
       string_list += element
     
     elsif element == array.at((array.length-1))
